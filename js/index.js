bf8e59779d12fd361c17bda9f4334bf9e8f7a4db
asdfggffxxfxhj <146187265+asdfggffxxfxhj@users.noreply.github.com>
2024-02-09 12:54:25 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||宁|慧|姐|姐|祝|你|新|年|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3|劳|文|启|祝|你|新|年|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
